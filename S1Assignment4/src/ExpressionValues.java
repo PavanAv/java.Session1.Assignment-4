9d1e1a59de95f6b0cdc66e60cb21eaa5a59f9221
PavanAv <pavanaluri2011@gmail.com>
2016-10-23 13:16:12 +0530

    session1assignment4
    session1assignment4

---
+++ S1Assignment4/src/ExpressionValues.java
@@ -0,0 +1,17 @@
+
+public class ExpressionValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x =5, y = 10,res;
+res = x + y * 2;
+System.out.println("Output of x+y*2 = "+ res);		
+res = x - y + 2;		
+System.out.println("Output of x-y+2 = "+ res);		
+res = (x+y)*2;		
+System.out.println("Output of (x+y)*2 = "+ res);		
+res = y%x;		
+System.out.println("Output of y%x = "+ res);
+	}
+
+}
